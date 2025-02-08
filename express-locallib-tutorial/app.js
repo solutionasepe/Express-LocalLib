6f0bf2406a92274f33bba3351c5fd696f4a737ac
solutionasepe <144459837+solutionasepe@users.noreply.github.com>
2025-02-08 11:14:38 +0100

    reolaced in app.js

--- express-locallib-tutorial/app.js
+++ express-locallib-tutorial/app.js
@@ -46,7 +46,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
