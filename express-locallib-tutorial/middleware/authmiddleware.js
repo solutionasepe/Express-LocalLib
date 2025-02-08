105773252725eef2c9acf8475151392fe44b4100
solutionasepe <144459837+solutionasepe@users.noreply.github.com>
2025-02-08 11:16:35 +0100

    finalized authorization

--- express-locallib-tutorial/middleware/authmiddleware.js
+++ express-locallib-tutorial/middleware/authmiddleware.js
@@ -4,16 +4,17 @@ const asyncHandler = require('express-async-handler');
 
 
 exports.Verify = asyncHandler (async function (req, res, next){
-    console.log({req})
+    console.log("Incoming request:", req.headers);
     // let token = req.cookies.SessionID;
-    const authheader = req.header['cookies'];
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    // if(!authHeader) return res.sendStatus(401);
+    if (!authHeader) return res.status(401).json({ message: "Unauthorized: No token provided" });
 
-
-    if(!authheader) return res.sendStatus(401);
-    // if (!token) return res.status(401).json({ message: "Unauthorized: No token provided" });
-
-    let cookie = authheader.split("=")[1];
-    jwt.verify(cookie, process.env.SECRET_ACCESS_TOKEN, async function (err, decoded){
+    const token =  authHeader.split(' ')[1];
+    console.log(token);
+    if(!token) return res.sendStatus(401);
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, async function (err, decoded){
         if (err) return res.status(400).json({message: "This session has expired pls login again"});
         // console.log(process.env.SECRET_ACCESS_TOKEN);
         let{userId} = decoded;
@@ -23,44 +24,29 @@ exports.Verify = asyncHandler (async function (req, res, next){
         console.log(`this is fine`)
         next();
     });
-    next()
 });
 
-// const util = require('util');
-// const jwtVerify = util.promisify(jwt.verify);
+exports.VerifyRole = function(userRole)
+{return asyncHandler(async function(req, res, next){console.log("Incoming request:", req.headers);
+    // let token = req.cookies.SessionID;
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    // if(!authHeader) return res.sendStatus(401);
+    if (!authHeader) return res.status(401).json({ message: "Unauthorized: No token provided" });
 
-// exports.Verify = asyncHandler(async function (req, res, next) {
-//     const token = req.cookies.SessionID;
+    const token =  authHeader.split(' ')[1];
+    console.log(token);
+    if(!token) return res.sendStatus(401);
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, async function (err, decoded){
+        if (err) return res.status(400).json({message: "This session has expired pls login again"});
+        // console.log(process.env.SECRET_ACCESS_TOKEN);
+        let{userId} = decoded;
+        let user = await User.findById(userId);
+        if(user.role !== userRole)
+            return res.status(401).json({message:"You're not authprised to view this page"});
+        next();
+    })
+    
+    });
+};
 
-//     if (!token) return res.status(401).json({ message: "Unauthorized: No token provided" });
-
-//     try {
-//         const decoded = await jwtVerify(token, process.env.SECRET_ACCESS_TOKEN);
-//         const { userId } = decoded;
-
-//         const user = await User.findById(userId);
-//         if (!user) return res.status(404).json({ message: "User not found" });
-
-//         const { password, ...userdata } = user._doc;
-//         req.user = userdata;
-
-//         next();
-//     } catch (err) {
-//         console.error(err);
-//         return res.status(400).json({ message: "This session has expired, please log in again" });
-//     }
-// });
-
-
-exports.VerifyRole = function(userRole){ 
-    return asyncHandler(async function(req, res, next){
-    const user = req.user;
-    const{role} = user;
-
-    if(role !== userRole){
-        return res.status(401).json({message:"You're not authprised to view this page"});
-    }
-
-    next();
-});
-}
--- express-locallib-tutorial/routes/users.js
+++ express-locallib-tutorial/routes/users.js
@@ -5,13 +5,16 @@ const auth_middleware = require('../middleware/authmiddleware');
 
 /* GET users listing. */
 router.post('/signup', user_controller.userSignup);
+
 router.get('/list', user_controller.userList);
+
 router.post('/login', user_controller.userLogin);
-// router.use(Verify)
+
 router.get('/dashboard', auth_middleware.Verify ,(req, res)=>{
   res.json({message: `welcome Asepe!`});
 });
-// router.get('/admindashboard', Verify, VerifyRole("admin"), function(req, res){
-  // res.status(200).json({message:"Welcome to admin portal"});
-// })
+
+router.get('/admindashboard',  auth_middleware.VerifyRole("admin"), function(req, res){
+  res.status(200).json({message:"Welcome to admin portal"});
+})
 module.exports = router;
